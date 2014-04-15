b2c8810884d26e79ded5538e5f405c646d217ea6
花田賢人 <hanadakent@hanadakenjin-no-Mac-Pro.local>
2014-04-15 18:16:41 +0900

    Fixed backup files name.

--- backup.php
+++ backup.php
@@ -3,7 +3,7 @@
 	$dbUser = 'root';
 	$dbPass = 'hk08336';
 	$dbName = 'contacttime';
-	$fileName = date('ymd').'_'.date('His').'.txt';
+	$fileName = date('ymd').'.txt';
 	$command = "mysqldump --default-character-set=binary ".$dbName." --host=".$dbHost." --user=".$dbUser." --password=".$dbPass." > backup/".$fileName;
 	system($command);
 ?>
