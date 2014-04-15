04e1627771c8c13607b540f6401fa44e038a415f
花田賢人 <hanadakent@hanadakenjin-no-Mac-Pro.local>
2014-04-15 19:21:12 +0900

    Added backup default shell

--- backup.sh
+++
@@ -1,6 +0,0 @@
-dbHost='localhost';
-dbUser='root';
-dbPass='hk08336';
-dbName='contacttime';
-fileName='backup/'`date '+%Y%m'`'.txt';
-mysqldump --default-character-set=binary $dbName --host=$dbHost --user=$dbUser --password=$dbPass > $fileName
