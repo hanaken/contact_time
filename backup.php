20441581820dd2ccade414a884968e437e150beb
Kent Hanada <k.hanada10@gmail.com>
2014-09-19 13:27:46 +0900

    change default file

--- backup.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$dbHost = 'localhost';
-	$dbUser = 'root';
-	$dbPass = 'hk08336';
-	$dbName = 'contacttime';
-	$fileName = date('ymd').'.txt';
-	$command = "mysqldump --default-character-set=binary ".$dbName." --host=".$dbHost." --user=".$dbUser." --password=".$dbPass." > backup/".$fileName;
-	system($command);
-?>
